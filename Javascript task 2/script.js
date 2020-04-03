81006516f5993c64b554b59d2ed0001d8ce0820b
simeon4real <simeon.udoh45@gmail.com>
2020-04-03 17:05:21 +0100

    :zap: Added comments to JS task 2 codes

--- Javascript task 2/script.js
+++ Javascript task 2/script.js
@@ -1,12 +1,16 @@
 let data, interestData;
 
+// An array of objects.
 data = [
 	{ principal: 2500, time: 1.8 },
 	{ principal: 1000, time: 5 },
 	{ principal: 3000, time: 1 },
 	{ principal: 2000, time: 3 }
 ];
+// A function to calculate the interest of "data" array
 function interestCalculator(arr) {
+	/*loop over and grab each element and their properties and check the conditions.
+	 */
 	arr.forEach(element => {
 		if (element.principal >= 2500 && element.time > 1 && element.time < 3) {
 			element.rate = 3;
@@ -19,6 +23,7 @@ function interestCalculator(arr) {
 		}
 		element.interest = (element.principal * element.rate * element.time) / 100;
 	});
+	// Using array.map() to create and store the results in "interestData"
 
 	interestData = data.map(element => {
 		return element;
@@ -27,6 +32,5 @@ function interestCalculator(arr) {
 	return interestData;
 }
 
+// initializing/calling the function.
 interestCalculator(data);
-
-
